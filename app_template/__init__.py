728fc4129c36ce7260212b6ef188bcdab53c96ce
qiusheng2011 <137812291@qq.com>
2023-12-06 20:28:44 +0800

    update

--- app_template/__init__.py
+++ app_template/__init__.py
@@ -1,16 +1,26 @@
 #! -*- coding:utf8 -*-
 
+from flask import Flask
+from dotenv import load_dotenv
 
+from .common import init_db_app
 
 class AppFactory():
 
-   @staticmethod 
-    def create_app(self, config_filename):
+    @staticmethod 
+    def create_app(env_file_prefix=""):
         app = Flask(__name__)
-        app.config.from_pyfile(config_filename)
-
-        ## 初始化数据库
-
+        if app.config['DEBUG']:
+            load_dotenv("../env/dev.env")
+        else:
+            load_dotenv("../env/prod.env")
+        app.config.from_prefixed_env("flask_app_template")
+        app.config['config_prefix'] = "flask_app_template"
+        
+        # 初始化 db
+        init_db_app(app)
         ## 引入视图
 
         return app
+
+app = AppFactory.create_app()
